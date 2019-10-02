2616b19468e80342d7261ce9c7be7ef070b3374d
Eloi Genier <eloig3357@5565.ca>
2019-10-01 20:06:19 -0600

    Added a bunch of stuff to the lawnmower

---
+++ LawnMowerRPG/Assets/Scripts/grass_behaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grass_behaviour : MonoBehaviour {
+
+    public Sprite notCut;
+    public Sprite cut;
+
+    private SpriteRenderer spriteRenderer;
+
+    public GameObject player;
+    Vector3 pos;
+    Vector3 playerPos;
+    private bool isCut = false;
+
+    // Use this for initialization
+    void Start () {
+
+        pos = transform.position; // Take the current position
+
+        spriteRenderer = GetComponent<SpriteRenderer>(); // We are accessing the SpriteRenderer that is attached to the Gameobject
+        if (spriteRenderer.sprite == null) // If the sprite on spriteRenderer is null then
+            spriteRenderer.sprite = notCut; // Set the sprite to sprite1
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerPos = player.transform.position;
+
+        if (pos == playerPos)
+        {
+            isCut = true;
+        }
+
+        if (isCut == true)
+        {
+            ChangeTheSprite(1);
+        }
+	}
+
+    void ChangeTheSprite(int cutStage)
+    {
+        if (cutStage == 0)
+        {
+            spriteRenderer.sprite = notCut;
+        }
+        if (cutStage == 1)
+        {
+            spriteRenderer.sprite = cut;
+        }
+        
+    }
+}
---
+++ LawnMowerRPG/Assets/Scripts/movement_controller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement_controller : MonoBehaviour {
+
+    Vector3 pos;
+    public float speed = 2.0f;
+    public int move_speed = 30;
+    private int move_timer = -1;
+
+    // Use this for initialization
+    void Start () {
+        pos = transform.position; // Take the current position
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        // Inputs
+        if (Input.GetKey(KeyCode.A) && transform.position == pos && Physics2D.Raycast(transform.position, Vector2.left, 16) == false && move_timer == -1)
+        {           //(-1,0)
+            pos += Vector3.left * 16;// Add -1 to pos.x
+            move_timer = 0;
+        }
+        if (Input.GetKey(KeyCode.D) && transform.position == pos && Physics2D.Raycast(transform.position, Vector2.right, 16) == false && move_timer == -1)
+        {           //(1,0)
+            pos += Vector3.right * 16;// Add 1 to pos.x
+            move_timer = 0;
+        }
+        if (Input.GetKey(KeyCode.W) && transform.position == pos && Physics2D.Raycast(transform.position, Vector2.up, 16) == false && move_timer == -1)
+        {           //(0,1)
+            pos += Vector3.up * 16; // Add 1 to pos.y
+            move_timer = 0;
+        }
+        if (Input.GetKey(KeyCode.S) && transform.position == pos && Physics2D.Raycast(transform.position, Vector2.down, 16) == false && move_timer == -1)
+        {           //(0,-1)
+            pos += Vector3.down * 16;// Add -1 to pos.y
+            move_timer = 0;
+        }
+
+        if (move_timer != -1)
+        {
+            move_timer += 1;
+        }
+        if (move_timer >= move_speed)
+        {
+            move_timer = -1;
+        }
+
+        //The Current Position = Move To (the current position to the new position by the speed * Time.DeltaTime)
+        transform.position = Vector3.MoveTowards(transform.position, pos, speed);    // Move there
+
+    }
+}
