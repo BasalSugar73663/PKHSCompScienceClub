82dfab71536847fb1f8636548d9d8aa14decafd3
Shipka69 <56271964+Shipka69@users.noreply.github.com>
2019-11-20 12:38:16 -0700

    Physics Prototype
    I added this physics prototype

---
+++ LawnMowerGame/Control Test (Physics)/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody playerBody;
+
+    private Vector3 inputVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVector = new Vector3(Input.GetAxis("Horizontal") * 10, playerBody.velocity.y, Input.GetAxis("Vertical") * 10);
+        transform.LookAt(transform.position + new Vector3(inputVector.x,0,inputVector.z));
+    }
+
+    private void FixedUpdate()
+    {
+        playerBody.velocity = inputVector;
+    }
+
+}
---
+++ LawnMowerGame/Control Test (Physics)/Assets/Scripts/PlayerDestroyGrass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDestroyGrass : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Grass Block")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+    
+
+}
